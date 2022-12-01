fbb8d76dc56a3be7627f0dc00837f8147128e66b
Sondre Aasemoen <sondre@eons.io>
2022-12-01 08:50:47 +0100

    In the beginning there was darkness

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import qualified AoC (someFunc)
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Haskell!"
+  AoC.someFunc
---
+++ src/Day/Day01.hs
@@ -0,0 +1,7 @@
+module Day.Day01 (
+    main
+) where
+
+main :: IO ()
+main = do 
+  print "Hello, World!"
