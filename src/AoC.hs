0a99f25e4c7a7f58df73961a0c97c758e021ded7
Sondre Aasemoen <sondre@eons.io>
2022-12-01 09:38:59 +0100

    Day 1 finished

--- src/AoC.hs
+++ src/AoC.hs
@@ -1,4 +1,49 @@
-module AoC (someFunc) where
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+module AoC where
+
+import Text.Printf (printf)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Control.Exception (throw)
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+inputName :: Int -> FilePath
+inputName = printf "inputs/day%02d.input" 
+
+exampleName :: Int -> FilePath
+exampleName = printf "inputs/day%02d.test" 
+
+getInput :: Int -> Parser a -> IO [a]
+getInput i p = do
+  input <- readFile (inputName i)
+  pure $ pLines p input
+
+getExampleInput :: Int -> Parser a -> IO [a]
+getExampleInput i p = do
+  input <- readFile (exampleName i)
+  pure $ pLines p input
+
+rawExampleInput :: Int -> IO Text 
+rawExampleInput day = T.strip <$> TIO.readFile (exampleName day)
+
+rawInput :: Int -> IO Text 
+rawInput day = T.strip <$> TIO.readFile (inputName day)
+
+type Parser = Parsec Void String
+
+number :: Integral a => Parser a
+number = L.signed (return ()) L.decimal
+
+pLines :: Parser a -> String -> [a]
+pLines parser input = case parse (traverse p (lines input)) "" input of
+  Left err -> throw err
+  Right a -> a
+  where
+    p l = setInput l *> parser <* eof <* setInput "\n" <* newline
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- src/Day/Day01.hs
+++ src/Day/Day01.hs
@@ -2,6 +2,22 @@ module Day.Day01 (
     main
 ) where
 
+import AoC (rawExampleInput, rawInput)
+import Data.Text qualified as T
+import Data.List (sort)
+
+partA :: [[Int]] -> Int 
+partA xs = maximum $ map sum xs
+
+partB :: [[Int]] -> Int 
+partB xs = sum $ take 3 $ reverse $ sort $ map sum xs
+
 main :: IO ()
 main = do 
-  print "Hello, World!"
+  input <- map (map read . words . T.unpack) . T.splitOn "\n\n" <$> rawInput 1
+  example <- map (map read . words . T.unpack) . T.splitOn "\n\n" <$> rawExampleInput 1
+  print (partA example)
+  print (partA input)
+
+  print (partB example)
+  print (partB input)
