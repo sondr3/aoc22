15eb611256fee384581e31446439e7267ab8b410
Sondre Aasemoen <sondre@eons.io>
2022-12-08 15:38:44 +0100

    Add example file to copy around

---
+++ src/day/dayXX.rs
@@ -0,0 +1,26 @@
+use crate::{tests, AoC};
+
+pub struct DayXX;
+
+impl AoC for DayXX {
+    type Output = todo!();
+    type Input = todo!();
+
+    fn day() -> usize {
+        todo!()
+    }
+
+    fn parse_one(input: &str) -> Self::Input {
+        todo!()
+    }
+
+    fn part_one(input: Self::Input) -> Self::Output {
+        todo!()
+    }
+
+    fn part_two(input: Self::Input) -> Self::Output {
+        todo!()
+    }
+}
+
+tests!(DayXX, 0, 0, 0, 0);
